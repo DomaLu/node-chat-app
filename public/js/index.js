6dcaf3983be4895970872266ab448eaf5f615862
Doma Lu <kaku18ccf52@gmail.com>
2017-09-27 14:24:11 +0800

    09-110 done

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,6 @@
 var socket = io()
 socket.on('connect', function() {
 	console.log('Connected to server')
-
-	socket.emit('createMessage', {
-		from: 'Darren',
-		text: 'Horray!',
-		createAt: Date.now()
-	})
 })
 socket.on('disconnect', function() {
 	console.log('Disconnected from server')
--- server/server.js
+++ server/server.js
@@ -15,13 +15,29 @@ io.on('connection', socket => {
 	console.log('New user connected')
 
 	socket.emit('newMessage', {
-		from: 'Server',
-		text: 'You mother fucker.',
-		createAt: Date.now()
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createAt: new Date().toLocaleString()
+	})
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createAt: new Date().toLocaleString()
 	})
 
 	socket.on('createMessage', message => {
 		console.log('createMessage', message)
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createAt: new Date().toLocaleString()
+		})
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createAt: new Date().toLocaleString()
+		// })
 	})
 
 	socket.on('disconnect', () => console.log('User was disconnected'))
